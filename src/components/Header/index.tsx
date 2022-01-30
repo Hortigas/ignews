0a40344cdc432480c79b9301c9dbebbaf241271e
Hortigas <xxvsvitor98xx@gmail.com>
2022-01-30 01:19:38 -0300

    CMS prismic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,21 @@
-import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+
+import { ActiveLink } from './ActiveLink';
+import { SignInButton } from '../SignInButton';
+import Image from 'next/image';
 
 export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
+                <Image src="/images/logo.svg" alt="ig.news" height="100px" width="100px" />
                 <nav>
-                    <a className={styles.active} href="">
-                        Home
-                    </a>
-                    <a href="">Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SignInButton />
             </div>
